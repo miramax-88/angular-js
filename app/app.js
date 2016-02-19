58d3e25dc41037ecbd49dccee647849749988b85
Maksym Kondratenko <maksym.kondratenko@gmail.com>
2016-02-19 14:10:28 +0200

    firs commit message

--- app/app.js
+++ app/app.js
@@ -7,11 +7,19 @@
 var myApp = angular.module('myApp', [
     'ngResource',
     'ngRoute',
-    'LocalStorageModule',
-    'myApp.view1',
-    'myApp.view2',
-    'myApp.version'
-]).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    'LocalStorageModule'
+]).config(function ($provide) {
+    $provide.decorator('$httpBackend', angular.mock.e2e.$httpBackendDecorator);
+}).run(function($httpBackend){
+    $httpBackend.whenGET('/partials/login.html').passThrough();
+    $httpBackend.whenGET('/partials/courses.html').passThrough();
+    $httpBackend.whenGET('/partials/directives/authorsList.html').passThrough();
+    $httpBackend.whenGET('/data/user/test.json').respond(
+        {"userName":"test","password":"test","name":"Max","emailAddress":"test@test.com"}
+    );
+});
+
+myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider) {
     $routeProvider.when('/login', {templateUrl: '/partials/login.html', controller: 'LoginController'});
     $routeProvider.when('/courses', {templateUrl: '/partials/courses.html', controller: 'CoursesController'});
     $routeProvider.when('/courses/new', {templateUrl: '/partials/course.html', controller: 'CourseController'});
--- app/filters/filters.js
+++ app/filters/filters.js
@@ -13,18 +13,3 @@ myApp.filter('duration', [function () {
     };
 }]);
 
-/*
-myApp.filter('formattedDate', function () {
-
-    return function (date) {
-        var reg = /\d{2}\/\d{2}\/\d{4}/;
-        var rez;
-        if (!date) {
-            return '';
-        }
-        rez = date.replace(reg, '');
-        console.log(rez)
-        return rez;
-    };
-});
-*/
