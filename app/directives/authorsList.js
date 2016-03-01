ac61f1038468a3e6c9e62ee9e8b93f59200ccdd7
Maksym Kondratenko <maksym.kondratenko@gmail.com>
2016-03-01 15:14:29 +0200

    firs commit message

--- app/directives/authorsList.js
+++ app/directives/authorsList.js
@@ -11,17 +11,15 @@ myApp.directive('authorsList', function ($location) {
         controller: function ($scope) {
             $scope.models = {
                 selected: null,
-                lists: {"A": [], "B": []}
+                lists: {"Course": [], "Authors": []}
             };
 
             // Generate initial model
             for (var i = 1; i <= 3; ++i) {
-                $scope.models.lists.A.push({label: "Item A" + i});
-                $scope.models.lists.B.push({label: "Item B" + i});
+                $scope.models.lists.Authors.push({label: "Author " + i});
             }
 
-            // Model to JSON for demo purpose
-            $scope.$watch('models', function (model) {
+            $scope.$watch('models', function(model) {
                 $scope.modelAsJson = angular.toJson(model, true);
             }, true);
         }
